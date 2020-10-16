eefcff5677ec47665eac908deab8d17a59434a3b
R1an <r1ankeel@gmail.com>
2020-10-16 15:32:00 +0300

    get api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    const url = 'https://api.generated.photos/api/v1/faces?api_key=';
+    const key = 'FlP2lwctf2UCWpVA6vTlqQ';
+    const [image, setImage] = useState('');
+
+    useEffect(() => {
+        axios.get(url + key)
+            .then(res => {
+                console.log(res.data.faces)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Yo maaan</h1>
     </div>
   );
 }
